9c04c4de65fc67718254a3893c82897da53ad82b
grvashu <grv@mathscapes.xyz>
2017-04-17 14:29:51 +0530

    updated map() function and startNotifications()

--- TwoSensorsValue/TwoSensorsValue.ino
+++ TwoSensorsValue/TwoSensorsValue.ino
@@ -1,7 +1,7 @@
 
 #include <CurieBLE.h>
 
-BLEPeripheral blePeripheral;       
+BLEPeripheral blePeripheral;
 BLEService pressureSensorService = BLEService ("181C");
 
 BLEUnsignedCharCharacteristic sensorOneReadingChar("2A80", BLERead | BLENotify);
@@ -13,46 +13,46 @@ int sensorOne, sensorTwo;
 String test, test1, test2;
 
 void setup() {
-  Serial.begin(9600);   
+  Serial.begin(9600);
   blePeripheral.setLocalName("ReRide");
   blePeripheral.setAdvertisedServiceUuid(pressureSensorService.uuid());
-  blePeripheral.addAttribute(pressureSensorService);   
-  blePeripheral.addAttribute(sensorOneReadingChar); 
-  blePeripheral.addAttribute(sensorTwoReadingChar); 
+  blePeripheral.addAttribute(pressureSensorService);
+  blePeripheral.addAttribute(sensorOneReadingChar);
+  blePeripheral.addAttribute(sensorTwoReadingChar);
   blePeripheral.begin();
   Serial.println("Bluetooth device active, waiting for connections...");
 }
 
-void loop() 
+void loop()
 {
 
   BLECentral central = blePeripheral.central();
 
   if (central) {
     Serial.print("Connected to central: ");
- 
+
     Serial.println(central.address());
 
     sensorOne = analogRead(0);
     sensorTwo = analogRead(1);
-  /*  test = String("Time :" , millis());
-    test1 = String("Sensor 1 :" , currentState);
-    test2 = String("Sensor 2 :" , fakeSensorValue);*/
+    /*  test = String("Time :" , millis());
+      test1 = String("Sensor 1 :" , currentState);
+      test2 = String("Sensor 2 :" , fakeSensorValue);*/
     Serial.print("Time");
     Serial.println(millis());
 
     Serial.print("Sensor 1 : ");
-    Serial.println(sensorOne);
+    Serial.println(map(sensorOne, 800, 1023, 0, 1023));
 
     Serial.print("Sensor 2 : ");
-    Serial.println(sensorTwo);
-    
+    Serial.println(map(sensorTwo, 800, 1023, 0, 1023));
+
     //Serial.println(test);
     //Serial.println(test1);
     //Serial.println(test2);
     sensorOneReadingChar.setValue(sensorOne);
     sensorTwoReadingChar.setValue(sensorTwo);
-    //fakeSensorValue++; 
+    //fakeSensorValue++;
     delay(1000);
   }
 }
